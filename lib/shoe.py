b3609b8709bf225c1887f33f4475c0b284842c9d
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-12-05 17:16:05 +0300

    Completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+
+    def get_brand(self):
+        return self._brand
+    def set_brand(self, value):
+        self._brand = value
+    def get_size(self):
+        return self._size
+    def set_size(self, value):
+        if type(value) in (int,):
+            self._size = value
+        else:
+            print("size must be an integer")
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+    brand = property(get_brand, set_brand)
+    size = property(get_size, set_size)
+    
